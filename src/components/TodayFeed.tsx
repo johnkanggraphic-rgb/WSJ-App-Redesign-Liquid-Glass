37c0164610d186ae8dc73c91729b4eb583910484
John <johnkanggraphic@gmail.com>
2026-07-03 00:15:07 -0400

    Add 8px spacer above opinion section

--- src/components/TodayFeed.tsx
+++ src/components/TodayFeed.tsx
@@ -683,6 +683,7 @@ export default function TodayFeed({ onArticleTap, onCommentTap }: { onArticleTap
         <FeedDivider />
         <NewsPackageCard />
         <FeedDivider />
+        <div style={{ height: 8, background: '#f4f5f7', flexShrink: 0, width: '100%' }} />
         <OpinionPackage />
         <FeedDivider />
         <WorldPackage />
